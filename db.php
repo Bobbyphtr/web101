ec88fc48dfbff5b6b2a27c14f24261ac988307d9
Xenoire <xeonexenoire@gmail.com>
2018-09-14 22:08:15 +0700

    Test phpcs, cuma tambah comment dan perapian php formal

--- db.php
+++ db.php
@@ -1,14 +1,24 @@
-<?php
+<?php 
+/**
+ * This is a formal php script --"  
+ * ni gara gara phpcs lgsg merah semua screen ku wwkwkw
+ *  
+ * PHP version 7
+ * 
+ * @category Nothing
+ * @package  Nothing
+ * @author   nothing <nothing@nothing.com>
+ * @license  nolisense URL
+ * @link     nothing
+ */
 
 $servername="localhost";
 $username="root";
 $password="";
 $database="leadership101";
-
-$conn=mysqli_connect($servername,$username, $password, $database);
-
+$conn=mysqli_connect($servername, $username, $password, $database);
 if (!$conn) {
-	die("Connection failed:". mysqli_connect_error());
+    die("Connection failed:". mysqli_connect_error());
 }
 // echo "connected successfully";
 ?>
